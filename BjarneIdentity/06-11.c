38a89c29d629658f6f5869f22370a961b275c778
Robert Ruetz <ruetz.robert@gmail.com>
2016-11-06 21:16:57 -0600

    Changed fahrenheit calculation.

--- BjarneIdentity/06-11.c
+++ BjarneIdentity/06-11.c
@@ -5,13 +5,13 @@ int main(void) {
 	double farenheit, kelvin;
 
 	int celsius = 0;
-	farenheit = (celsius * (9.0 / 5.0)) + 32.0;
+	farenheit = celsius * 9/5 + 32.0;
 	kelvin = celsius + 273;
 	printf("%d celsius = %.2f farenheit\n", celsius, farenheit);
 	printf("%d celsius = %.2f kelvin\n", celsius, kelvin);
 
 	celsius = 100.0;
-	farenheit = (celsius * (9.0 / 5.0)) + 32.0;
+	farenheit = celsius * 9 / 5 + 32.0;
 	kelvin = celsius + 273;
 	printf("%d celsius = %.2f farenheit\n", celsius, farenheit);
 	printf("%d celsius = %.2f kelvin\n", celsius, kelvin);
